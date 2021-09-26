4b6da7727968dfec1b0653e7bede7e25d1594551
Jevon Charles <jevoncharles@me.com>
2021-09-26 19:02:57 -0400

    Initial Commit

---
+++ USSpending/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  USSpending
+//
+//  Created by Jevon Charles on 9/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ USSpending/USSpendingApp.swift
@@ -0,0 +1,17 @@
+//
+//  USSpendingApp.swift
+//  USSpending
+//
+//  Created by Jevon Charles on 9/26/21.
+//
+
+import SwiftUI
+
+@main
+struct USSpendingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
